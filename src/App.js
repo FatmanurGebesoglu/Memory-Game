db44e01a4d857863de78010284a69d20ccd42d58
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-17 11:44:53 +0300

    project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+const kartResimler=[
+  {"src":"/img/box.png"},
+  {"src":"/img/flower1.png"},
+  {"src":"/img/flower2.png"},
+  {"src":"/img/flower3.png"},
+  {"src":"/img/flower4.png"},
+  {"src":"/img/flower5.png"},
+  {"src":"/img/flower6.png"},
+]
 
 function App() {
+
+  const [ kartlar, setKartlar] = useState([]);
+
+  const karistir = () => {
+    const karistirilmiskartlar=[...kartResimler,...kartResimler].sort(()=>
+    Math.random()-0.5).map((k)=>({...k,id:Math.random()}))
+
+    setKartlar(karistirilmiskartlar)
+  }
+
+  console.log(kartlar);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MEMORY GAME</h1>
+      <button onClick={karistir}>
+        YENİ OYUN
+      </button>
     </div>
   );
 }
